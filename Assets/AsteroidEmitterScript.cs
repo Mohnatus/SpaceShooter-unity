2be1b469c82e4874343a0c72f12a58d5e5ca200f
korneeva-m <korneeva-ms@yandex.ru>
2019-03-27 18:14:20 +0300

    day 2

---
+++ Assets/AsteroidEmitterScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidEmitterScript : MonoBehaviour
+{
+
+    public float minDelay;
+    public float maxDelay;
+
+    public GameObject asteroid;
+
+    private float nextSpawn;
+    private float asteroidCount = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            float maxAsteroid = Random.Range(1, asteroidCount);
+
+            for (int i = 0; i < maxAsteroid; i++)
+            {
+                nextSpawn = Time.time + Random.Range(minDelay, maxDelay);
+                float randomXPosition = Random.Range(-transform.localScale.x / 2, transform.localScale.x / 2);
+                float randomZPosition = transform.position.z + Random.Range(0, 5);
+
+                Vector3 startPosition = new Vector3(
+                    randomXPosition,
+                    transform.position.y,
+                    randomZPosition
+                );
+
+                Instantiate(asteroid, startPosition, Quaternion.identity);
+            }
+
+            asteroidCount += 0.3f;
+        }
+    }
+}
---
+++ Assets/BgScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroller : MonoBehaviour
+{
+    public float speed; // скорость прокрутки фона
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * speed, transform.localScale.y);
+        transform.position = startPosition + Vector3.back * newPosition;
+    }
+}
